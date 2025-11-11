c704b54fc393502e759edb66000ca6087e72cefe
Sorin Vijoaica <sorin.vijoaica@gmail.com>
2025-11-11 15:09:42 +0800

    Set metadata and icon

--- src/main/java/com/gimhiscores/combinedgimhiscores/CombinedGIMHiscoresPlugin.java
+++ src/main/java/com/gimhiscores/combinedgimhiscores/CombinedGIMHiscoresPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.gimhiscores.combinedgimhiscores;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class CombinedGIMHiscoresPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private CombinedGIMHiscoresPluginConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    CombinedGIMHiscoresPluginConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(CombinedGIMHiscoresPluginConfig.class);
 	}
 }
